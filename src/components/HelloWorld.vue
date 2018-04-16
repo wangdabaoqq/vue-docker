e76216c5b6620e92d64106b6d6995e7d004c5207
wangdabaoqq <614692339@qq.com>
2018-04-16 16:15:00 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your First Docker App'
+      msg: 'Welcome to Your First Docker App Update'
     }
   }
 }
